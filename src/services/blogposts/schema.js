1f2ab9ca7a960152252b130f83f2213a19adf8b1
MSajedian <mohammadsajedian@gmail.com>
2021-06-02 13:31:20 +0100

    removed useless codes

--- src/services/blogposts/schema.js
+++ src/services/blogposts/schema.js
@@ -2,46 +2,6 @@ import mongoose from "mongoose"
 
 const { Schema, model } = mongoose
 
-// const BlogpostSchema = new Schema({
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   cover: {
-//     type: String,
-//     required: true,
-//   },
-//   readTime: {
-//     value: {
-//       type: Number,
-//       required: true,
-//     },
-//     unit: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   author: {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     avatar: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   content: {
-//     type: String,
-//     required: true,
-//   },
-//   { timestamps: true }
-// )
-
 const BlogpostSchema = new Schema(
   {
     category: {
